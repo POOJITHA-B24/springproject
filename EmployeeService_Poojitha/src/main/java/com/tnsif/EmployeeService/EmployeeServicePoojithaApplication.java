f78e54664d43088aeb3dd2124ace14ddfff7eb8e
pooja <pooja@LAPTOP-6KMQL8U3>
2024-04-06 21:15:24 +0530

    this is employee service page for shopping mall project

---
+++ EmployeeService_Poojitha/src/main/java/com/tnsif/EmployeeService/EmployeeServicePoojithaApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.EmployeeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServicePoojithaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServicePoojithaApplication.class, args);
+	}
+
+}
---
+++ EmployeeService_Poojitha/src/test/java/com/tnsif/EmployeeService/EmployeeServicePoojithaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.EmployeeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServicePoojithaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
